0b5a08f845876039b12051a2426b81fcc2352e00
dzdaniel84 <dzdaniel84@gmail.com>
2017-01-21 16:02:32 -0800

    Finally works

--- bg.js
+++ bg.js
@@ -1,3 +1,15 @@
-chrome.tabs.onUpdated.addListener(function() {
-  alert('updated from background');
+function recordSearches(url) {
+	if (url.includes("google.com/search")) {
+		url = url.split("?q=");
+		url = url[1].split("&oq=")[0];
+		url = url.replace("+", " ");
+		alert(url);
+	}
+}
+
+chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab) {
+   chrome.extension.getBackgroundPage().console.log(tab.url);
+   if (changeInfo.status == "complete"){
+   	recordSearches(tab.url);
+   }
 });
