8241585d19d7f66a29e58b652544807719968c5c
dzdaniel84 <dzdaniel84@gmail.com>
2017-01-21 16:23:50 -0800

    It works perfectly now

--- bg.js
+++ bg.js
@@ -1,15 +1,19 @@
 function recordSearches(url) {
-	if (url.includes("google.com/search")) {
-		url = url.split("?q=");
-		url = url[1].split("&oq=")[0];
-		url = url.replace("+", " ");
-		alert(url);
+	url = url.split("?q=")[1];
+	if (url.includes("#q=")){
+		url = url.split("#q=")[1];
+	} else {
+		url = url.split("&oq=")[0];
 	}
+	while (url.includes("+")){
+		url = url.replace("+", " ");
+	}
+	alert(url);
 }
 
 chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab) {
    chrome.extension.getBackgroundPage().console.log(tab.url);
-   if (changeInfo.status == "complete"){
+   if (changeInfo.status == "complete" && tab.url.includes("google.com/search")){
    	recordSearches(tab.url);
    }
 });
